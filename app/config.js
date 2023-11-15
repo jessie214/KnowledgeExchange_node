92bf686543e8d9b86ad2fbf54e138e0c1bd47652
JessieYan214 <jessieyan214@gmail.com>
2023-11-15 16:49:52 +1300

    initialize

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr:'mongodb+srv://jessieyan214:admin1234@mymangodb-cluster.xrhdkil.mongodb.net/?retryWrites=true&w=majority'
+}
--- app/index.js
+++ app/index.js
@@ -4,6 +4,17 @@ const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const app = new Koa();
 const routing = require('./routes');
+const mongoose = require('mongoose');
+
+const { connectionStr } = require('./config');
+mongoose.connect(connectionStr, { useNewUrlParser: true})
+  .then(() => {
+    console.log('MongoDB connection established');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
 
 app.use(error({
   postFormat:(e,{stack,...rest})=>process.env.NODE_ENV === 'production'? rest:{stack, ...rest}
