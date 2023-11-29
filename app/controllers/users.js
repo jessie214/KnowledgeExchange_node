6f1f7f68c180784aa2b50a9d134337386f77868f
JessieYan214 <jessieyan214@gmail.com>
2023-11-29 13:07:37 +1300

    added users model

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,38 +1,36 @@
-const db=[{name:'jessie'}]
+const User = require('../models/users');
+
+const db = [{ name: 'jessie' }]
 class UsersCtl {
-  find(ctx) {
-    ctx.body = db
+  async find(ctx) {
+    ctx.body = await User.find();
   }
-  findById(ctx) { 
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412);
-    }
-    ctx.body = db[ctx.params.id *1];
+  async findById(ctx) { 
+    const user = await User.findById(ctx.params.id);
+    if (!user) { ctx.throw(404, 'user not found'); }
+    ctx.body = user;
   }
-  create(ctx) { 
+  
+  async create(ctx) { 
+  ctx.verifyParams({
+    name: { type: 'string', required: true },
+  });
+  const user = await new User(ctx.request.body).save();
+  ctx.body = user;
+  }
+  async update(ctx) {    
     ctx.verifyParams({
       name: { type: 'string', required: true },
-      age:{type:'number',required:false}
     })
-    db.push(ctx.request.body)
-    ctx.body=ctx.request.body
-  }
-  update(ctx) { 
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412);
+    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+    if (!user) {
+      ctx.throw(404,'user not found');
     }
-    ctx.verifyParams({
-      name: { type: 'string', required: true },
-      age:{type:'number',required:false}
-    })
-    db[ctx.params.id * 1] = ctx.request.body;
-    ctx.body =ctx.request.body
+    ctx.body = user;
   }
-  deleteUser(ctx) { 
-    if (ctx.params.id * 1 >= db.length) {
-      ctx.throw(412);
-    }
-    db.splice(ctx.params.id *1, 1);
+  async deleteUser(ctx) { 
+    const user = await User.findByIdAndDelete(ctx.params.id);
+    if(!user) {ctx.throw(404,'user not found')}
     ctx.status = 204;
   }
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,11 @@
 const Router = require('koa-router');
 const router = new Router({ prefix: '/users' });
-const {find,findById,create,update,deleteUser} = require('../controllers/users')
+const { find, findById, create, update, deleteUser } = require('../controllers/users');
 
 router.get('/', find)
 router.post('/',create)
 router.get('/:id', findById)
 router.put('/:id',update)
-router.delete('/:id', deleteUser)
+router.delete('/:id',deleteUser)
 
 module.exports = router;
