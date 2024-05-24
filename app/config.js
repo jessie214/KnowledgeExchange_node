b481c74b202bde28ee952a8c9b4eac5b9dfa007e
Jessie Yan <45481191+jessie214@users.noreply.github.com>
2024-05-24 18:07:56 +1200

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   secret:'zhihu-jwt-secret', // the password never write here
-  connectionStr:'mongodb+srv://jessieyan214:admin1234@mymangodb-cluster.xrhdkil.mongodb.net/?retryWrites=true&w=majority'
+  connectionStr:'mongodb+srv://username:password@mymangodb-cluster.xrhdkil.mongodb.net/?retryWrites=true&w=majority'
 }
